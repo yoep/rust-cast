2b53e27391a27d1019d02a78d65570c96b074570
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2016-06-30 00:07:08 +0200

    Simply Chromecast structure.

--- src/lib.rs
+++ src/lib.rs
@@ -55,105 +55,50 @@ pub enum ChromecastApps {
 }
 
 pub struct Chromecast {
-    host: String,
-    port: u16,
-    pub stream: Option<Rc<RefCell<SslStream<TcpStream>>>>,
+    stream: Rc<RefCell<SslStream<TcpStream>>>,
 
-    pub heartbeat: Option<HeartbeatChannel<SslStream<TcpStream>>>,
-    pub connection: Option<ConnectionChannel<SslStream<TcpStream>>>,
-    pub receiver: Option<ReceiverChannel<SslStream<TcpStream>>>,
+    pub heartbeat: HeartbeatChannel<SslStream<TcpStream>>,
+    pub connection: ConnectionChannel<SslStream<TcpStream>>,
+    pub receiver: ReceiverChannel<SslStream<TcpStream>>,
 }
 
 impl Chromecast {
-    pub fn new(host: String, port: u16) -> Chromecast {
-        Chromecast {
-            host: host,
-            port: port,
-            stream: None,
-            heartbeat: None,
-            connection: None,
-            receiver: None,
-        }
-    }
-
-    pub fn connect(&mut self) -> Result<(), Error> {
+    pub fn connect(host: String, port: u16) -> Result<Chromecast, Error> {
         let ssl_context = try!(SslContext::new(SslMethod::Sslv23));
-        let tcp_stream = try!(TcpStream::connect((self.host.as_ref(), self.port)));
+        let tcp_stream = try!(TcpStream::connect((host.as_ref(), port)));
         let ssl_stream = try!(SslStream::connect(&ssl_context, tcp_stream));
 
         let ssl_stream_rc = Rc::new(RefCell::new(ssl_stream));
 
-        self.heartbeat = Some(HeartbeatChannel::new(DEFAULT_SENDER_ID.to_owned(),
-                                                    DEFAULT_RECEIVER_ID.to_owned(),
-                                                    ssl_stream_rc.clone()));
-        self.connection = Some(ConnectionChannel::new(DEFAULT_SENDER_ID.to_owned(),
-                                                      ssl_stream_rc.clone()));
-        self.receiver = Some(ReceiverChannel::new(DEFAULT_SENDER_ID.to_owned(),
-                                                  DEFAULT_RECEIVER_ID.to_owned(),
-                                                  ssl_stream_rc.clone()));
-
-        self.stream = Some(ssl_stream_rc);
-
-        Ok(())
+        let heartbeat = HeartbeatChannel::new(DEFAULT_SENDER_ID.to_owned(),
+                                              DEFAULT_RECEIVER_ID.to_owned(),
+                                              ssl_stream_rc.clone());
+        let connection = ConnectionChannel::new(DEFAULT_SENDER_ID.to_owned(),
+                                                ssl_stream_rc.clone());
+        let receiver = ReceiverChannel::new(DEFAULT_SENDER_ID.to_owned(),
+                                            DEFAULT_RECEIVER_ID.to_owned(),
+                                            ssl_stream_rc.clone());
+        Ok(Chromecast {
+            stream: ssl_stream_rc,
+            heartbeat: heartbeat,
+            connection: connection,
+            receiver: receiver,
+        })
     }
 
-    pub fn receive(&mut self) -> Result<cast_channel::CastMessage, Error> {
-        if self.stream.is_none() {
-            return Err(Error::Internal("Chromecast is not connected!".to_owned()));
-        }
-
-        let stream = self.stream.as_ref().unwrap();
-        let message = MessageManager::receive(&mut *stream.borrow_mut());
+    pub fn receive(&self) -> Result<cast_channel::CastMessage, Error> {
+        let message = MessageManager::receive(&mut *self.stream.borrow_mut());
 
         debug!("Message received: {:?}", message);
 
         Ok(message)
     }
 
-    pub fn create_heartbeat_channel(&self)
-        -> Result<HeartbeatChannel<SslStream<TcpStream>>, Error> {
-        if self.stream.is_none() {
-            return Err(Error::Internal("Chromecast is not connected!".to_owned()));
-        }
-
-        let stream = self.stream.as_ref().unwrap();
-
-        Ok(HeartbeatChannel::new(DEFAULT_SENDER_ID.to_owned(),
-                                 DEFAULT_RECEIVER_ID.to_owned(),
-                                 stream.clone()))
-    }
-
-    pub fn create_connection_channel(&self)
-        -> Result<ConnectionChannel<SslStream<TcpStream>>, Error> {
-        if self.stream.is_none() {
-            return Err(Error::Internal("Chromecast is not connected!".to_owned()));
-        }
-
-        let stream = self.stream.as_ref().unwrap();
-        Ok(ConnectionChannel::new(DEFAULT_SENDER_ID.to_owned(), stream.clone()))
-    }
-
-    pub fn create_receiver_channel(&self) -> Result<ReceiverChannel<SslStream<TcpStream>>, Error> {
-        if self.stream.is_none() {
-            return Err(Error::Internal("Chromecast is not connected!".to_owned()));
-        }
-
-        let stream = self.stream.as_ref().unwrap();
-        Ok(ReceiverChannel::new(DEFAULT_SENDER_ID.to_owned(),
-                                DEFAULT_RECEIVER_ID.to_owned(),
-                                stream.clone()))
-    }
-
     pub fn create_media_channel(&self, receiver: String, session_id: String)
         -> Result<MediaChannel<SslStream<TcpStream>>, Error> {
-        if self.stream.is_none() {
-            return Err(Error::Internal("Chromecast is not connected!".to_owned()));
-        }
-
-        let stream = self.stream.as_ref().unwrap();
         Ok(MediaChannel::new(DEFAULT_SENDER_ID.to_owned(),
                              receiver,
                              session_id,
-                             stream.clone()))
+                             self.stream.clone()))
     }
 }
